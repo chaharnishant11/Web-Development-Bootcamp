51a4a41c54b79914ea309701429a6bb9e0453773
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-02-15 00:41:06 +0530

    Adding tempelate

---
+++ React/react-app-tempelate/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+      return(
+            <div>
+              React App
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
