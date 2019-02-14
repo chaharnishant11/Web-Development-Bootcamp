230dac0d5d50ea22badca5c9e56234cb75e4d355
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-02-14 21:40:53 +0530

    Adding React

---
+++ React/starter/node_modules/stylehacks/dist/exists.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = exists;
+function exists(selector, index, value) {
+    const node = selector.at(index);
+
+    return node && node.value.toLowerCase() === value;
+}
+module.exports = exports["default"];
